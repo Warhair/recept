8632ca7e8030b1a0ca0cae9260ab9ac9f62b80a3
Warhair <tegzesmarton@gmail.com>
2024-04-12 14:15:34 +0200

    recept_database

---
+++ alap_breeze-master/app/Http/Controllers/KategoriaController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\kategoria;
+use Illuminate\Http\Request;
+
+class KategoriaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\kategoria  $kategoria
+     * @return \Illuminate\Http\Response
+     */
+    public function show(kategoria $kategoria)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\kategoria  $kategoria
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(kategoria $kategoria)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\kategoria  $kategoria
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, kategoria $kategoria)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\kategoria  $kategoria
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(kategoria $kategoria)
+    {
+        //
+    }
+}
---
+++ alap_breeze-master/app/Models/receptek.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class receptek extends Model
+{
+    use HasFactory;
+    protected $primaryKey="id";
+	protected $fillable = ['nev','kat_id','kep_eleresi_ut','leiras'];
+}
